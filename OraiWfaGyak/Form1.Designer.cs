fa307d17cf175ddf00f2ecfc6a909fe96428d0f9
asd <asd>
2018-10-05 16:18:14 +0200

    OraiMunka

--- OraiWfaGyak/Form1.Designer.cs
+++ OraiWfaGyak/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sorokSzama = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sorokSzamaLbl = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sorokSzama)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sorokSzama
+            // 
+            this.sorokSzama.Location = new System.Drawing.Point(95, 12);
+            this.sorokSzama.Name = "sorokSzama";
+            this.sorokSzama.Size = new System.Drawing.Size(96, 20);
+            this.sorokSzama.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sorokSzamaLbl
+            // 
+            this.sorokSzamaLbl.AutoSize = true;
+            this.sorokSzamaLbl.Location = new System.Drawing.Point(12, 14);
+            this.sorokSzamaLbl.Name = "sorokSzamaLbl";
+            this.sorokSzamaLbl.Size = new System.Drawing.Size(77, 13);
+            this.sorokSzamaLbl.TabIndex = 2;
+            this.sorokSzamaLbl.Text = "Sorok száma : ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "WriteAll";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.sorokSzamaLbl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sorokSzama);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sorokSzama)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown sorokSzama;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label sorokSzamaLbl;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- OraiWfaGyak/Form1.cs
+++ OraiWfaGyak/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,63 @@ namespace OraiWfaGyak
 {
     public partial class Form1 : Form
     {
+        List<TextBox> txtBoxList = new List<TextBox>();
+
+
         public Form1()
         {
             InitializeComponent();
-            this.Text = "CímLeL";
+            this.Text = "Pogram 4Head";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int db = (int)sorokSzama.Value;
+            for (int i = 0; i < db; i++)
+            {
+                TextBox dynamicTxtBox = new TextBox();
+                dynamicTxtBox.SetBounds(10, 50 + i * 25, 100, dynamicTxtBox.Height);
+                this.Controls.Add(dynamicTxtBox);
+                this.txtBoxList.Add(dynamicTxtBox);
+
+                Button b = new Button();
+                b.SetBounds(125, 50 + i * 25, 100, b.Height);
+                b.Text = "Button " + (i + 1);
+                this.Controls.Add(b);
+                b.MouseUp += (sndr1, args1) =>
+                {
+                    if (args1.Button == MouseButtons.Right)
+                    {
+                        MessageBox.Show("JobbKlikkeltél!");
+
+
+                    }
+                    if (args1.Button == MouseButtons.Left)
+                    {
+                        MessageBox.Show("Tartalom: " + dynamicTxtBox.Text);
+                    }
+                };
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string szoveg = "";
+            foreach (var tb in txtBoxList)
+            {
+                szoveg += tb.Text + Environment.NewLine;
+            }
+            MessageBox.Show(szoveg);
+            if (File.Exists("gyak.txt"))
+            {
+                File.AppendAllText("gyak.txt", DateTime.Now + Environment.NewLine + szoveg + Environment.NewLine);
+            }
+            else
+            {
+                File.WriteAllText("gyak.txt", DateTime.Now + Environment.NewLine + szoveg + Environment.NewLine);
+            }
+
         }
     }
 }
